8cfb00738767ecfefd5bd17074aeaeab26d011cb
Mahbub Hassan <mahbubhassanmunna@gmail.com>
2025-10-26 01:46:59 +0600

    added optimal solution to group angram

--- medium/group_angram.js
+++ medium/group_angram.js
@@ -17,4 +17,26 @@ function groupAnagram(strs) {
   return Array.from(map.values());
 }
 
-console.log(groupAnagram(["eat", "tea", "tan", "ate", "nat", "bat"]));
+function groupAnagramOptimal(strs) {
+  const map = new Map();
+  for (const word of strs) {
+    const freq = new Array(26).fill(0);
+
+    for (const char of word) {
+      const index = char.charCodeAt(0) - "a".charCodeAt(0);
+      freq[index]++;
+    }
+
+    const key = freq.join("#");
+
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+
+    map.get(key).push(word);
+  }
+
+  return Array.from(map.values());
+}
+
+console.log(groupAnagramOptimal(["eat", "tea", "tan", "ate", "nat", "bat"]));
